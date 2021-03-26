156eaeecc9602494af9406537ccc4e12396c2451
maria-luiza-duda <marialuizaduda00@gmail.com>
2021-03-26 09:55:26 -0300

    Laço de repetição For

---
+++ for.js
@@ -0,0 +1,19 @@
+for (const iterator of object) {
+    
+}
+
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+
+array.forEach(element => {
+    
+});
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
